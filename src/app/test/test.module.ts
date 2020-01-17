19be8f4be3d7656aea4aa2585d1d99f47e8dadb5
tareku <tarekbenbrahim1@gmail.com>
2020-01-17 21:16:22 +0100

    added custom component

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { TestPageRoutingModule } from './test-routing.module';
 
 import { TestPage } from './test.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     TestPageRoutingModule
   ],
   declarations: [TestPage]
