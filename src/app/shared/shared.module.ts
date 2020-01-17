ccad014418c20cea99c8dd6660acef5dd340f849
tareku <tarekbenbrahim1@gmail.com>
2020-01-17 20:23:53 +0100

    updated shared module with first and second custom component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
+
+import { FirstCustomComponentComponent } from './first-custom-component/first-custom-component.component';
+import { SecondCustomComponentComponent } from './second-custom-component/second-custom-component.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    ProfileComponent
+    FirstCustomComponentComponent,
+    SecondCustomComponentComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    LoginComponent, 
-    ProfileComponent
+    FirstCustomComponentComponent,
+    SecondCustomComponentComponent
   ]
 })
 export class SharedModule { }
