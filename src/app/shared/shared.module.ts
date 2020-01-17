4ed9b6d669434cff275b86df1ccbcaa20260a156
tareku <tarekbenbrahim1@gmail.com>
2020-01-17 19:58:00 +0100

    added profile and login components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    ProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    LoginComponent, 
+    ProfileComponent
   ]
 })
 export class SharedModule { }
